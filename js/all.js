0bf6b5933b37c1ffe1b3f862bfc53cee0e065c11
jacky123789456@gmail.com <jacky123789456@gmail.com>
2021-05-26 20:09:11 +0800

    login finished

---
+++ js/all.js
@@ -0,0 +1,41 @@
+Vue.createApp({
+    data(){
+        return {
+            
+        }
+    },
+    methods: {
+        signin(){
+            const api_path = 'jacky';
+            const url = 'https://vue3-course-api.hexschool.io/';
+
+            const login = document.querySelector("#login");
+            const emailInput = document.querySelector("#username");
+            const passwordInput = document.querySelector("#password");
+
+            const username = emailInput.value;
+            const password = passwordInput.value;
+            const data = {
+                username, password
+            }
+
+            axios.post(`${url}admin/signin`, data).then(function(res) {
+                console.log(res);
+                if(res.data.success){
+                    const { token , expired } = res.data; //一種解構手法 (所寫)，當確定 dtat 裡面有這個值得時候就可以這樣寫
+            // 將 token 存到 cookie~
+            document.cookie = `hexToken=${token}; expires=${new Date(expired)}`;
+            window.location = 'admin.html';
+                }else{
+                    alert('請輸入正確的帳號密碼');
+                }
+            }).catch((err) => {
+                console.log(err);
+            })
+            console.log(data);
+        }
+    },
+    // mounted() {
+    //     app = new bootstrap.    
+    // }
+}).mount('#app');
