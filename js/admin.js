9f85d2f9dac1c4c9479c924e644e569c0d29aacb
jacky123789456@gmail.com <jacky123789456@gmail.com>
2021-05-27 21:59:46 +0800

    open modal need to fix

--- js/admin.js
+++ js/admin.js
@@ -1,34 +1,49 @@
-let productModal;
-let delProductModal;
+let productModal = null;
+let delProductModal = null;
 
 Vue.createApp({
     data(){
         return {
             products: [],
+            isNew: false,
+            tempProduct: {
+                imagesUrl: [],
+            }
         }
     },
     methods: {
         getData(){
             const api_path = 'jacky';
             const url = 'https://vue3-course-api.hexschool.io/';
-            
+
             axios.get(`${url}api/${api_path}/admin/products`).then((res) => {
                 console.log(res);
                 this.products = res.data.products;
                 console.log(this.products);
             })
         },
-        renderList() {
+        // renderList() {
             
-        },
+        // },
         openModal(){
+            console.log('click');
+            productModal.show();
 
         }
     },
     mounted() {
+        productModal = new bootstrap.Modal(document.getElementById('productModal'), {
+            keyboard: false
+        });
+
+        delProductModal = new bootstrap.Modal(document.getElementById('delProductModal'), {
+            keyboard: false
+        });
+
         let token = document.cookie.replace(/(?:(?:^|.*;\s*)hexToken\s*\=\s*([^;]*).*$)|^.*$/, "$1");
         axios.defaults.headers.common['Authorization'] = token;
         console.log(token);
         this.getData();
+        // this.openModal();
     }
 }).mount("#app")
