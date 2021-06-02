138a6216fbb5fd3a1b1e49ebb1d07314826ac034
jacky123789456@gmail.com <jacky123789456@gmail.com>
2021-06-02 18:23:31 +0800

    completed adjust 1

--- js/all.js
+++ js/all.js
@@ -7,31 +7,37 @@ Vue.createApp({
     },
     methods: {
         signin(){
-            const login = document.querySelector("#login");
-            const emailInput = document.querySelector("#username");
-            const passwordInput = document.querySelector("#password");
-
-            const username = emailInput.value;
-            const password = passwordInput.value;
-            const data = {
-                username, password
-            }
-
-            axios.post(`${this.url}admin/signin`, data).then(function(res) {
-                console.log(res);
-                if(res.data.success){
-                    const { token , expired } = res.data; //一種解構手法 (所寫)，當確定 dtat 裡面有這個值得時候就可以這樣寫
-                    // 將 token 存到 cookie~
-                    document.cookie = `hexToken=${token}; expires=${new Date(expired)}`;
-                    window.location = 'admin.html';
-                }else{
-                    alert('請輸入正確的帳號密碼');
-                    window.location = 'index.html';
+            form.addEventListener('click', (e) => {
+                console.log(e.target.nodeName == 'BUTTON');
+                if(e.target.nodeName == 'BUTTON'){
+                    const form = document.querySelector("#form");
+                    const login = document.querySelector("#login");
+                    const emailInput = document.querySelector("#username");
+                    const passwordInput = document.querySelector("#password");
+        
+                    const username = emailInput.value;
+                    const password = passwordInput.value;
+                    const data = {
+                        username, password
+                    }
+        
+                    axios.post(`${this.url}admin/signin`, data).then(function(res) {
+                        console.log(res);
+                        if(res.data.success){
+                            const { token , expired } = res.data; //一種解構手法 (所寫)，當確定 dtat 裡面有這個值得時候就可以這樣寫
+                            // 將 token 存到 cookie~
+                            document.cookie = `hexToken=${token}; expires=${new Date(expired)}`;
+                            window.location = 'admin.html';
+                        }else{
+                            alert('請輸入正確的帳號密碼');
+                            window.location = 'index.html';
+                        }
+                    }).catch((err) => {
+                        console.log(err);
+                    })
+                    console.log(data);
                 }
-            }).catch((err) => {
-                console.log(err);
             })
-            console.log(data);
         }
     }
 }).mount('#app');
